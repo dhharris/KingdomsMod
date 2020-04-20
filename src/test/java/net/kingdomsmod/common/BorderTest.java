0c4a12d10212c308e3d3da170fbe2fcf1ddc67a1
Hugh Harris <dhharris9@gmail.com>
2020-04-19 23:02:33 -0700

    Rename test cases

--- src/test/java/net/kingdomsmod/common/BorderTest.java
+++ src/test/java/net/kingdomsmod/common/BorderTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class BorderTest {
 
     @Test
-    void isWithinBorderShouldBeTrue() {
+    void TestIsWithinBorderShouldBeTrue() {
         Border border = new Border(new BlockPos(0, 0, 0), new BlockPos(100,0,100));
         BlockPos myBlock = new BlockPos(50, 100, 50);
 
@@ -17,7 +17,7 @@ class BorderTest {
     }
 
     @Test
-    void isWithinBorderShouldBeFalse() {
+    void TestIsWithinBorderShouldBeFalse() {
         Border border = new Border(new BlockPos(0, 0, 0), new BlockPos(100,0,100));
         BlockPos myBlock = new BlockPos(-50, 0, 50);
 
