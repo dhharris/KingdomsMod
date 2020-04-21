f5466dd10db6c0f4cc1a7795ee68fc4f6401c317
Hugh Harris <dhharris9@gmail.com>
2020-04-20 23:34:27 -0700

    Add list of taxable blocks

--- src/main/java/net/kingdomsmod/common/TaxCollector.java
+++ src/main/java/net/kingdomsmod/common/TaxCollector.java
@@ -1,24 +1,32 @@
 package net.kingdomsmod.common;
 
-import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Hashtable;
 
 public class TaxCollector {
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final HashSet<Item> taxableItems = new HashSet<>(Arrays.asList(
+            Items.GOLD_ORE,
+            Items.IRON_ORE,
+            Items.DIAMOND_ORE,
+            Items.REDSTONE_ORE,
+            Items.LAPIS_ORE
+    )); // Note: using HashSet provides better perf with O(1) contains()
 
 
     // Maps players to their taxable income
     // TODO: Need to store this so it doesn't reset when a player logs out
-    private Hashtable<PlayerEntity, ItemCounter> taxableIncome = new Hashtable<PlayerEntity, ItemCounter>();
+    private Hashtable<PlayerEntity, ItemCounter> taxableIncome = new Hashtable<>();
 
     boolean isTaxable(Item item) {
-        return true;
+        return taxableItems.contains(item);
     }
 
     public void addIncome(PlayerEntity player, Item item) {
@@ -29,7 +37,6 @@ public class TaxCollector {
             }
             ItemCounter playerIncome = taxableIncome.get(player);
             playerIncome.add(Item.getIdFromItem(item));
-            LOGGER.info(player.getName().getString() + "'s income now at " + playerIncome.get(item) + " for item " + item);
         }
     }
 }
